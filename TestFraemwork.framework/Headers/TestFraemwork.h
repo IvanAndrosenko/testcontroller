0222e1717ba554a779882652771213c4ae4c3ac4
Ivan Androsenko <Ivan@Ivans-MBP.Dlink>
2016-07-01 17:17:06 +0500

    6

---
+++ TestFraemwork.framework/Headers/TestFraemwork.h
@@ -0,0 +1,19 @@
+//
+//  TestFraemwork.h
+//  TestFraemwork
+//
+//  Created by Ivan Androsenko on 01.07.16.
+//  Copyright © 2016 Ivan Androsenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFraemwork.
+FOUNDATION_EXPORT double TestFraemworkVersionNumber;
+
+//! Project version string for TestFraemwork.
+FOUNDATION_EXPORT const unsigned char TestFraemworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFraemwork/PublicHeader.h>
+
+
